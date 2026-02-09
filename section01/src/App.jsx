62f327e188ef0d4b9ce26d09a6c71804b987e23c
hy3oning <shyun5635@gmail.com>
2026-02-10 00:29:14 +0900

    [create] useState 사용해보기

---
+++ section01/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Bulb from "./components/Blub";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <Bulb />
+      <Counter />
+    </>
+  );
+}
+
+export default App;
